663caf7e339591ecd8022b57fcc6df0e5bd50b6c
BennoSchmidt <benno.schmidt@hs-bochum.de>
2015-05-11 14:25:59 +0200

    Test

--- src/org/n52/v3d/worldviz/demoapplications/HelloWorldViz.java
+++ src/org/n52/v3d/worldviz/demoapplications/HelloWorldViz.java
@@ -3,7 +3,7 @@ package org.n52.v3d.worldviz.demoapplications;
 public class HelloWorldViz {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Triturus WorldViz!!");
+		System.out.println("Hello, Triturus WorldViz!");
 	}
 	
 }
