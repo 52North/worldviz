f5b2c825acd3259ca5c63e5ef8999c892c92c2f4
Christian Danowski <christian.danowski@hs-bochum.de>
2015-07-07 10:44:08 +0200

    added class comment

--- src/main/java/org/n52/v3d/worldviz/projections/NormTransform_Wgs84.java
+++ src/main/java/org/n52/v3d/worldviz/projections/NormTransform_Wgs84.java
@@ -7,6 +7,22 @@ import java.util.List;
 import org.n52.v3d.triturus.gisimplm.GmPoint;
 import org.n52.v3d.triturus.vgis.VgPoint;
 
+/**
+ * This is a subclass of {@link NormTransform} that is only applicable to
+ * coordinates in coordinate reference system WGS84 (Longitude, Latitude,
+ * Altitude).<br/>
+ * <br/>
+ * 
+ * When transforming WGS84 coordinates via this class, the normalization of
+ * coordinates will be executed within a fixed spatial extent. This fixed
+ * spatial extent refers to the bounding box of possible WGS84-coordinates. <br/>
+ * <br/>
+ * The bounding box is given by the points (X,Y,Z = Longitude, Latitude,
+ * Altitude): <code>LowerLeft(-180, -90, 0), TopRight(180, 90, 0)</code>.
+ * 
+ * @author Christian Danowski
+ *
+ */
 public class NormTransform_Wgs84 extends NormTransform {
 
 	public NormTransform_Wgs84(VgPoint[] geoPos) {
@@ -23,8 +39,8 @@ public class NormTransform_Wgs84 extends NormTransform {
 	 * specialized subclass of {@link NormTransform} works with a fixed spatial
 	 * extent. </br></br>
 	 * 
-	 * The bounding box contains the points (X,Y,Z = Longitude, Latitude,
-	 * Altitude): <code>(-180, -90, 0), (180, 90, 0)</code>.
+	 * The bounding box is given by the points (X,Y,Z = Longitude, Latitude,
+	 * Altitude): <code>LowerLeft(-180, -90, 0), TopRight(180, 90, 0)</code>.
 	 * 
 	 * @param geoPos
 	 *            to this collection of points the two points
@@ -52,8 +68,8 @@ public class NormTransform_Wgs84 extends NormTransform {
 	 * specialized subclass of {@link NormTransform} works with a fixed spatial
 	 * extent. </br></br>
 	 * 
-	 * The bounding box contains the points (X,Y,Z = Longitude, Latitude,
-	 * Altitude): <code>(-180, -90, 0), (180, 90, 0)</code>.
+	 * The bounding box is given by the points (X,Y,Z = Longitude, Latitude,
+	 * Altitude): <code>LowerLeft(-180, -90, 0), TopRight(180, 90, 0)</code>.
 	 * 
 	 * @param geoPos
 	 *            to this collection of points the two points
