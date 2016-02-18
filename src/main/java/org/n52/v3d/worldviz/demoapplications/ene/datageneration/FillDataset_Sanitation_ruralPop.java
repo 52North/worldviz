ac35251d45b2b9cd271745a89e5eb6eea979734b
Christian Danowski <christian.danowski@hs-bochum.de>
2016-02-18 09:56:11 +0100

    add new data: drinking water, sanitation (each for rural, urban and total population)

---
+++ src/main/java/org/n52/v3d/worldviz/demoapplications/ene/datageneration/FillDataset_Sanitation_ruralPop.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (C) 2015-2015 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *  - Apache License, version 2.0
+ *  - Apache Software License, version 1.0
+ *  - GNU Lesser General Public License, version 3
+ *  - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *  - Common Development and Distribution License (CDDL), version 1.0.
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public
+ * License version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ */
+package org.n52.v3d.worldviz.demoapplications.ene.datageneration;
+
+import java.io.FileNotFoundException;
+
+import org.n52.v3d.worldviz.dataaccess.importtools.CsvImporter;
+import org.n52.v3d.worldviz.helper.RelativePaths;
+
+/**
+ * This class generates the Entries part of the 'CarbonEmissions.xml'-file.
+ * 
+ * @author Christian Danowski
+ * 
+ */
+public class FillDataset_Sanitation_ruralPop {
+
+	public static void main(String[] args) {
+		String xmlFileLocation = RelativePaths.SANITATION_RURAL_POPULATION_XML;
+		String csvFileLocation = RelativePaths.SANITATION_RURAL_POPULATION_CSV;
+		Character csvSeperator = ',';
+		String[] csvHeaders = new String[] { "ISO 2 Code", "1990", "1991", "1992", "1993", "1994", "1995", "1996",
+				"1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+				"2010", "2011", "2012" };
+		String nullValue = "NULL";
+		String zeroValue = "NODATA";
+		int numberOfInitLinesToSkip = 4;
+
+		CsvImporter fillSanitation_ruralPop = new CsvImporter(xmlFileLocation, csvFileLocation, csvSeperator, csvHeaders,
+				nullValue, zeroValue, numberOfInitLinesToSkip);
+		try {
+			fillSanitation_ruralPop.fillDatasetEntries();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
